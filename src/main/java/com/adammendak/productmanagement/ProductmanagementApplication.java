0e159dd8a1f4c43dc26d4bf3fc4624e190853fb1
adammendak <adam.mendak@gmail.com>
2017-12-30 15:25:35 +0100

    first commit

---
+++ src/main/java/com/adammendak/productmanagement/ProductmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.adammendak.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanagementApplication.class, args);
+	}
+}
