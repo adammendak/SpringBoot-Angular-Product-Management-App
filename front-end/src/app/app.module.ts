37a10797b6944378a5eea6f4ace46ea2b17b067e
adammendak <adam.mendak@gmail.com>
2017-12-30 16:48:48 +0100

    more front

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
