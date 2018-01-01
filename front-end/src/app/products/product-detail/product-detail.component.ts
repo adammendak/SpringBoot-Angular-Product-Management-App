0954b42447964568a14892641f3e10b68d9336d6
adammendak <adam.mendak@gmail.com>
2018-01-01 20:16:36 +0100

    Added

--- front-end/src/app/products/product-detail/product-detail.component.ts
+++ front-end/src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from "../product";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-detail',
@@ -12,10 +12,14 @@ export class ProductDetailComponent implements OnInit {
   pageTitle :string = "Product Detail";
   product: IProduct;
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id');
+  }
+
+  onBack() :void {
+    this._router.navigate['/products'];
   }
 
 }
