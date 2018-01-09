fcf99b8eb60b2d1a3a166f4d80ad51168e81d837
adammendak <adam.mendak@gmail.com>
2018-01-09 19:44:02 +0100

    added user

---
+++ src/main/java/com/adammendak/productmanagement/model/User.java
@@ -0,0 +1,18 @@
+package com.adammendak.productmanagement.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+}
