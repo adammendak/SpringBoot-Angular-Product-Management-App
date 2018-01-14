6d55a14c45e395f7cf6b18ffee5c229a20fb4571
adammendak <adam.mendak@gmail.com>
2018-01-14 21:03:52 +0100

    added

--- src/main/java/com/adammendak/productmanagement/model/dto/ProductDto.java
+++ src/main/java/com/adammendak/productmanagement/model/dto/ProductDto.java
@@ -1,7 +1,14 @@
 package com.adammendak.productmanagement.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
 
     private Long id;
