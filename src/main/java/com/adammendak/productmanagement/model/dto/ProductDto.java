b3b8d438c7978e953df55c7d87b80a8549fcc8f7
adammendak <adam.mendak@gmail.com>
2018-02-23 16:40:15 +0100

    added json property

--- src/main/java/com/adammendak/productmanagement/model/dto/ProductDto.java
+++ src/main/java/com/adammendak/productmanagement/model/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package com.adammendak.productmanagement.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +15,14 @@ public class ProductDto {
     private Long id;
     private String name;
     private String code;
+    @JsonProperty(value = "release_date")
     private String releaseDate;
+    @JsonProperty(value = "blockade_date")
     private String blockadeDate;
     private BigDecimal price;
+    @JsonProperty(value = "star_rating")
     private Double starRating;
+    @JsonProperty(value = "image_url")
     private String imageUrl;
 
 }
