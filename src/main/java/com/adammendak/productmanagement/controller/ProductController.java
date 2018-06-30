71e3d3a6bf2163046fa63d2b0c52e31d4b1d1aad
adammendak <adam.mendak@gmail.com>
2018-06-30 15:04:33 +0200

    working status v1.0

--- src/main/java/com/adammendak/productmanagement/controller/ProductController.java
+++ src/main/java/com/adammendak/productmanagement/controller/ProductController.java
@@ -6,8 +6,6 @@ import com.adammendak.productmanagement.repository.ProductRepository;
 import com.adammendak.productmanagement.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
